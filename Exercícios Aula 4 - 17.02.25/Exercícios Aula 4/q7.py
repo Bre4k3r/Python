bd569f3a015f7a6d5f3703c1c62491784ccedbb1
Breaker <125100283+Bre4k3r@users.noreply.github.com>
2025-02-24 19:24:23 -0300

    17.02.25
    Aula 4

---
+++ Exercícios Aula 4 - 17.02.25/Exercícios Aula 4/q7.py
@@ -0,0 +1,8 @@
+print('=====FAZENDA=====')
+alqueires = int(input(f'Quantos alqueires a fazenda possuí?\nQuantidade: ',))
+
+viagem = (alqueires*250)/18
+viagem = round(viagem)
+
+print(f'\nSera necessário [{viagem}] viagens')
+print('================')
