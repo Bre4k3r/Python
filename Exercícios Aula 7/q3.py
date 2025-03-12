666d4557eba1aa33ab539c23c9d629ad15e7271b
Breaker <125100283+Bre4k3r@users.noreply.github.com>
2025-03-12 20:55:03 -0300

    Delete Exercícios Aula 7 directory

--- Exercícios Aula 7/q3.py
+++
@@ -1,5 +0,0 @@
-def area(l):
-    return(L*L)
-
-L = int(input('Digite o tamanho do lado em cm: '))
-print(f'A area é: {area(L)}cm²')
--- Exercícios Aula 7/q7.py
+++
@@ -1,7 +0,0 @@
-def soma_imposto(TaxaImposto, Custo):
-    return print(f'O valor após taxa é: {Custo*(1-(TaxaImposto/100))}')
-
-TaxaImposto = int(input('Digite a taxa de imposto: '))
-Custo = float(input('Digite o custo do produto: ').replace(',','.'))
-
-soma_imposto(TaxaImposto, Custo)
